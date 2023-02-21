ffa12fcd615138aa5cc9332cea35b722a0f6ecd0
Rihab Nasri <rihab.nasri@esprit.tn>
2023-02-21 12:44:27 +0100

    CRUD2

---
+++ src/main/java/tn/esprit/spring/repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package tn.esprit.spring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import tn.esprit.spring.entities.Role;
+import tn.esprit.spring.entities.Role;
+@Repository
+public interface RoleRepository extends JpaRepository<Role, String> {
+}
